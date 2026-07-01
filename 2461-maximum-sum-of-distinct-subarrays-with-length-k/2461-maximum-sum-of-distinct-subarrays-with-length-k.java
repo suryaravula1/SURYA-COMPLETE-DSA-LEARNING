c5a6f7b3a45f45547e9c21912d1c1fbc880862a7
Surya Prakash Ravula <72310895+suryaravula1@users.noreply.github.com>
2026-07-01 08:30:17 -0700

    Time: 69 ms (5.93%), Space: 104.3 MB (6.28%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,36 +1,46 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        Set<Integer> st = new HashSet<>();
+        
+        int left =0;
+        int right =0;
 
-        int r=0;
-        int l=0;
-        long sum=0;
-        long res=0;
-        while(r<nums.length){
-            if(!st.contains(nums[r])){
-                sum+=nums[r];
-                st.add(nums[r]);
-                
-                if((r-l+1)==k){
-                    res= Math.max(sum,res);
-                    sum-=nums[l];
-                    st.remove(nums[l]);
-                    l++;
-                }
-            
-            }else{
-                while(nums[l]!=nums[r]){
-                    sum-=nums[l];
-                    st.remove(nums[l]);
-                    l++;
-                }
-                l++;
+        HashMap<Integer, Integer> windowMap = new HashMap<>();
+        int dulicateCount =0;
+
+        long currentSum =0;
+        long maxSum =0;
+
+        while(right< nums.length){
+
+            currentSum += nums[right];
+
+            windowMap.put(nums[right], windowMap.getOrDefault(nums[right],0)+1);
+
+            if(windowMap.get(nums[right]) > 1){
+                dulicateCount++;
             }
-      
-            r++;
+            if(right -left+1 > k){
+                int leftNum = nums[left];
+
+                if(windowMap.get(leftNum)>1){
+                    dulicateCount--;
+                }
+
+                currentSum -= leftNum;
+                windowMap.put(leftNum, windowMap.get(leftNum)-1);
+                left++;
+            }
+
+
+            if(right - left + 1==k && dulicateCount==0){
+                maxSum = Math.max(maxSum, currentSum);
+            }
+
+            right++;
 
         }
 
-        return res;
+        return maxSum;
+
     }
 }
